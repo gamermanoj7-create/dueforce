91bf6547e5d49010d8452929704423073faa179a
gamermanoj7-create <271037619+gamermanoj7-create@users.noreply.github.com>
2026-09-24 02:55:27 +0530

    Clean old ToolPlay files from DUEFORCE

--- service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.registration.unregister()).then(()=>self.clients.matchAll()).then(cs=>cs.forEach(c=>c.navigate(c.url)))));
